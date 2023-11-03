a51ce0af0983346f8a1ef9825c780a39a29473e0
ahmedraza <ahmed.raza1054@gmail.com>
2023-11-03 19:49:23 +0500

    stopwatch ready

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
-var getMinutes = document.getElementById("min");
-var getSeconds = document.getElementById("sec");
-var getMiliSeconds = document.getElementById("mSec");
+var getMinutes = document.getElementById("min"),
+  getSeconds = document.getElementById("sec"),
+  getMiliSeconds = document.getElementById("mSec");
 
-var minutes = 0 ;
-var seconds = 0 ;
-var miliSeconds = 0 ;
+var startBtn = document.getElementById("start").style;
+var stopBtn = document.getElementById("stop").style;
+var resetBtn = document.getElementById("reset");
+
+var minutes = 0;
+var seconds = 0;
+var miliSeconds = 0;
 var timer;
 
 function startTimer() {
+  startBtn.display = "none";
+  stopBtn.display = "inline-block";
   timer = setInterval(() => {
     miliSeconds++;
-    getMiliSeconds.innerHTML = miliSeconds;
+    getMiliSeconds.innerHTML = miliSeconds + `<span>ms</span>`;
     if (miliSeconds >= 100) {
       seconds++;
-      getSeconds.innerHTML = seconds;
+      getSeconds.innerHTML = seconds + `<span>s</span>`;
       miliSeconds = 0;
     } else if (seconds > 5) {
       minutes++;
-      getMinutes.innerHTML = minutes;
+      getMinutes.innerHTML = minutes + `<span>m</span>`;
       seconds = 0;
     }
   }, 10);
 }
 
 function stopTimer() {
+  resetBtn.disabled = false;
+  startBtn.display = "inline-block";
+  stopBtn.display = "none";
   clearInterval(timer);
 }
 function resetTimer() {
-  minutes = 0;
   seconds = 0;
+  minutes = 0;
   miliSeconds = 0;
-  getMinutes.innerHTML = minutes;
-  getSeconds.innerHTML = seconds;
-  getMiliSeconds.innerHTML = miliSeconds;
+  getMinutes.innerHTML = minutes + `<span>m</span>`;
+  getSeconds.innerHTML = seconds + `<span>s</span>`;
+  getMiliSeconds.innerHTML = miliSeconds + `<span>ms</span>`;
+  resetBtn.disabled = true;
 }
